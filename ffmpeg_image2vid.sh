b74da48fe5e800bef1fa2dbc71c29ce9644effc0
Dante-Basile <dbasile1070@gmail.com>
2020-06-25 13:07:14 -0700

    added comments

--- ffmpeg_image2vid.sh
+++ ffmpeg_image2vid.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
 ffmpeg -framerate 30 -i "MyDir/myIm_%04d.png" -r 30 -vcodec libx264 -pix_fmt yuv420p -crf 23 myVid.mp4
+# framerate: input framerate
+# r: output framerate
+# crf: 23 is default?
